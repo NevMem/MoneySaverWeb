0ee0f7de72e19e0088554b88dd37a5a2bb4f5a37
NevMem <memlolkek@gmail.com>
2019-02-17 21:02:15 +0300

    Code refactoring

--- src/components/ModalPopup.js
+++ src/components/ModalPopup.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react'
 
 class ModalPopup extends Component {
-    constructor(prps) {
-        super(prps)
-        console.log(this.props)
-    }
-
-    render() {
-        if (!this.props.visible) return null
-
-        return (
-            <div className = 'modalWrapper'>
-                <div className = 'modalFrame'>
-                    <div className = 'modalHeader row'>
-                        <div>{this.props.header}</div>
-                        <div className = 'modal-close-btn' onClick = {this.props.close}>close</div>
-                    </div>
-                    <div className = 'modalContent'>{this.props.renderContent()}</div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    if (!this.props.visible) return null
+    return (
+      <div className = 'modalWrapper'>
+        <div className = 'modalFrame'>
+          <div className = 'modalHeader row'>
+            <div>{this.props.header}</div>
+            <div className = 'modal-close-btn' onClick = {this.props.close}>close</div>
+          </div>
+          <div className = 'modalContent'>{this.props.renderContent()}</div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ModalPopup
